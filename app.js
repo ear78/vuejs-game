ea93383a259b9b38dcc944f938283eb46c63e2b6
elliot richardson <ear78@aol.com>
2018-04-11 23:41:11 -0500

    refactored code for damage control

--- app.js
+++ app.js
@@ -11,6 +11,51 @@ new Vue({
             this.gameIsRunning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
+        },
+        attack: function(){
+            var damage = calculateDamage(3,10)
+            this.monsterHealth -= damage;
+
+            if(this.checkWin()){
+                return;
+            }
+
+            damage = calculateDamage(5,12)
+            this.playerHealth -= damage;
+            this.checkWin();
+        },
+        specialAttack: function(){
+
+        },
+        heal: function(){
+
+        },
+        giveUp: function(){
+
+        },
+        calculateDamage: function(min,max){
+            return Math.max(Math.floor(Math.random() * max) + 1,  min);
+        },
+        checkWin: function(){
+            if (this.monsterHealth <= 0) {
+                if (confirm('You won! New Game?')){
+                    this.startGame();
+                } else {
+                    this.gameIsRunning = false;
+                }
+                return true;
+                alert('You won!');
+                this.gameIsRunning = false;
+                return;
+            } else if (this.playerHealth <= 0){
+                if (confirm('You lost! New Game?')) {
+                    this.startGame();
+                } else {
+                    this.gameIsRunning = false;
+                }
+                return true;
+            }
+            return false;
         }
     }
 })
